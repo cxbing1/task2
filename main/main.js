8052c2fbeb08c4b52d6ea19ffc0cf042d4095a28
cxbing1 <983121062@qq.com>
2017-06-01 14:45:08 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,12 +1,6 @@
 // Write your cade below:
-module.exports = function main() {
-	function get_yu(a,b)
-	{
-		return a%b;
-	}
-
-	var c=get_yu(9,4);
-	var c=get_yu(9,3);
+module.exports = function main(a,b) {
 	
-  
+	return a%b;
+	
 };
